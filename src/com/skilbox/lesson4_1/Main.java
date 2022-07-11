73285f93daac92672eec2eaeb31a4800e8e045da
6firefox9 <vk_whitefox@icloud.com>
2022-07-11 19:44:26 +0300

    Решение задачи 4-6

--- src/com/skilbox/lesson4_1/Main.java
+++ src/com/skilbox/lesson4_1/Main.java
@@ -1,9 +1,29 @@
 package com.skilbox.lesson4_1;
 
+
 public class Main {
     public static void main(String[] args) {
-        public Integer sumDigits(Integer number){
+        Integer first = 12345;
+        Integer second = 10;
+        Integer third = 5059191;
 
-        }
+        System.out.println("Сумма цифр " + first + " равна " + sumDigits(first));
+        System.out.println("Сумма цифр " + second + " равна " + sumDigits(second));
+        System.out.println("Сумма цифр " + third + " равна " + sumDigits(third));
+        //Дополнительное задание*
+        char a1 = '8';
+        int a2 = Integer.parseInt(String.valueOf(a1));
+        System.out.println("Перевод символа '" + a1 + "' в целое число: " + a2);
     }
+
+    public static int sumDigits(Integer number){
+        String digitStr = number.toString();
+        int result = 0;
+        for (int i = 0; i < digitStr.length(); i++) {
+            result += Integer.parseInt(String.valueOf(digitStr.charAt(i)));
+        }
+        return result;
+    }
+
+
 }
